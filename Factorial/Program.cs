26747a522b7872ad00bf5ad66aed7383441d8c23
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:35:50 +0530

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number greater than or equal to zero: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factoria of " + x + " is: " + Factorial(x));
+        }
+    }
+}
